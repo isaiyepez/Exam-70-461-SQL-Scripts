ddc5696c23fb3062c306df2a7fa1df9d60cd2cf0
Isaí Yepez Gómez <isaiyepez@gmail.com>
2019-11-05 22:22:04 -0600

    Beginning indexes section

---
+++ Session 7/Session 7/Re-introducing Query plans.sql
@@ -0,0 +1,13 @@
+USE [70-461S7]
+GO
+
+SELECT [EmployeeNumber]
+      ,[EmployeeFirstName]
+      ,[EmployeeMiddleName]
+      ,[EmployeeLastName]
+      ,[EmployeeGovernmentID]
+      ,[DateOfBirth]
+      ,[Department]
+  FROM [dbo].[tblEmployee]
+GO
+
